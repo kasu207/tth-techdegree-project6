78405a44762ebff7fee09635db6e0a1d2cc452d3
Kasu207 <phlpp.rchtr@protonmail.com>
2020-08-24 22:58:25 +0200

    wip

--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { data } = require('../data.json');
+const { data } = require('../data/data.json');
 const { projects } = data;
 
 router.get( '/', ( req, res ) => {
@@ -10,7 +10,17 @@ router.get( '/', ( req, res ) => {
 });
 router.get('/:id', (req, res) => {
     const { id } = req.params;
+    const { project_name, description, technologies, live_link, github_link, image_urls } = projects[id];
+
+
     const templateData = { id };
+    templateData.project_name = project_name;
+    templateData.description = description;
+    templateData.technologies = technologies;
+    templateData.live_link = live_link;
+    templateData.github_link = github_link;
+    templateData.image_urls = image_urls;
+
     res.render('project', templateData);
 });
 
