31c0c02b2782db4a32b2e354a857559036244487
Kasu207 <phlpp.rchtr@protonmail.com>
2020-09-02 21:55:26 +0200

    Exteneded Version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # tth-techdegree-project6
  
+Extended:
+
+1# Package-json
+## added express
+## pug
+
+2# Error Page
+## Added error.pug with individual style
+
+3# CSS
+## changed portfolio-me : background color
+## changed portfolio-e a:hover: to a brighter color
+## changed heading font: to 'Roboto'
--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use((req, res, next) => {
     next(err);
   });
   
-app.use((err, req, res, next) => {
-    err.message = err.message || 'Unfortunately, something went wrong!';
+  app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
