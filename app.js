288d6ac08fb81f2b88c53335629e1b31b2be9713
Kasu207 <phlpp.rchtr@protonmail.com>
2020-09-07 21:56:03 +0200

    Error adjustment

--- app.js
+++ app.js
@@ -14,19 +14,21 @@ app.use('/projects', projectRoutes);
 
 //Errors
 app.use((req, res, next) => {
-    const err = new Error('I am sorry, the page you\'ve requested, could not be found');
-    err.status = 404;
-    next(err);
-  });
-  
-  app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
-  });
+  const err = new Error('I am sorry, the page you\'ve requested, could not be found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || "Looks like there was a problem on the server";
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 
 //localhost Path
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!')
+  console.log('The application is running on localhost:3000!')
 });
