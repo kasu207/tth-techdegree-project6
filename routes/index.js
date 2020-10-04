1dc1d447f23e4cf8ecb25c3f7409a5fbba923735
Kasu207 <phlpp.rchtr@protonmail.com>
2020-10-04 23:02:35 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ const { projects } = require('../data/data.json');
 
 //index Page
 router.get('/', (req, res) => {
-    console.log(projects.find( project => project.id === 1));
     res.render('index', { projects });
 });
 
 //projects Page(s)
 router.get('/projects/:id', (req, res, next) => {
-   const projectId = parseInt(req.params.id);
-   console.log(typeof 1);
+   const projectId = req.params.id;
    //const project = projects.find( ({ id }) => id === +projectId );
    const project = projects.find(project => project.id === projectId);
    if (project) {
